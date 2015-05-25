408a3b09558ee2c5396a91aa0af7d270e6c2fd89
Vlasov Artem <vlasovartem21@gmail.com>
2015-05-26 01:47:21 +0300

    Change project structure, move content in one folder. Delete test java file

--- src/main/java/razborpoletov/reader/utils/Constants.java
+++ src/main/java/razborpoletov/reader/utils/Constants.java
@@ -7,14 +7,15 @@ import java.util.List;
  * Created by artemvlasov on 22/05/15.
  */
 public class Constants {
+    private final static String BASE_CONTENT_URL = "content/";
     public final static String MARKDOWN_FORMAT = "markdown";
     public final static String ASCII_DOC = "adoc";
     public final static String HTML = "html";
-    public final static String CONFERENCES_FILE = "conferences.json";
-    public final static String USEFUL_THINGS_FILE = "useful-things.json";
-    public final static String PROJECT_STATISTICS_FILE = "project-statistics.json";
-    public final static String CONF_COMPLETE = "complete-conferences.json";
-    public final static String USEFUL_THINGS_COMPLETE = "complete-useful-things.json";
+    public final static String CONFERENCES_FILE = BASE_CONTENT_URL + "conferences.json";
+    public final static String USEFUL_THINGS_FILE = BASE_CONTENT_URL + "useful-things.json";
+    public final static String PROJECT_STATISTICS_FILE = BASE_CONTENT_URL + "project-statistics.json";
+    public final static String CONF_COMPLETE = BASE_CONTENT_URL + "complete-conferences.json";
+    public final static String USEFUL_THINGS_COMPLETE = BASE_CONTENT_URL + "complete-useful-things.json";
     public final static String[] PROP_NAMES = {"podcasts.folder", "local.git.folder"};
     public final static String LAST_PODCAST_PROP_NAME = "last.podcast";
     public final static String PODCASTS_COUNT_PROP_NAME = "podcasts.count";
