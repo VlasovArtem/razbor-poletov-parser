365da1c4e377200d4d79a8d345cd0badb119c2a6
Vlasov Artem <avlasov@selectica.com>
2017-07-04 19:40:55 +0300

    Add PodcastLink entity

---
+++ src/main/java/org/avlasov/razborpoletov/reader/entity/PodcastLink.java
@@ -0,0 +1,30 @@
+package org.avlasov.razborpoletov.reader.entity;
+
+/**
+ * Created by artemvlasov on 04/07/2017.
+ */
+public class PodcastLink {
+
+    private int podcastNumber;
+    private String link;
+
+    public PodcastLink(int podcastNumber, String link) {
+        this.podcastNumber = podcastNumber;
+        this.link = link;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PodcastLink)) return false;
+
+        PodcastLink that = (PodcastLink) o;
+
+        return podcastNumber == that.podcastNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return podcastNumber;
+    }
+}
