92d20bbb19e2b9c44fc1969898fd2b818b1dcdfa
Vlasov Artem <avlasov@selectica.com>
2017-07-04 14:25:18 +0300

    Move files to the new package

---
+++ src/main/java/org/avlasov/razborpoletov/reader/converters/AsciidocConverter.java
@@ -0,0 +1,210 @@
+package org.avlasov.razborpoletov.reader.converters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+/**
+ * Created by artemvlasov on 04/06/15.
+ */
+public class AsciidocConverter {
+    private final static File PODCAST_FOLDER = new File("/Users/artemvlasov/git/razbor-poletov.github.com/source/_posts/");
+    private final static Logger LOG = LoggerFactory.getLogger(AsciidocConverter.class);
+//    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+//        String commandPattern = "pandoc -f %s -t asciidoc -o %s %s";
+//        StatisticParser parser = new StatisticParser();
+//        for (File file : Preconditions.checkNotNull(PODCAST_FOLDER.listFiles(), "File list folder is empty")) {
+//            if(Pattern.matches("20([0-9]{2}-){3}episode-[0-9].+|(2013-02-03-episode33-aws-event\\.markdown)", file
+//                    .getName())) {
+//                String from;
+//                String input = file.getAbsolutePath();
+//                String[] command;
+//                PodcastFile podcastFile = null;
+//                switch (FilenameUtils.getExtension(file.getName())) {
+//                    case Constants.MARKDOWN_FORMAT:
+//                        podcastFile = new PodcastFile(file);
+//                        from = "markdown";
+//                        command = String.format(commandPattern, from, podcastFile.getOutputFile(), input).split(" ");
+//                        executePandocCommand(command);
+//                        podcastFile.setAsciidocWithoutHtml(prepareAsciidocStringConvertedFromMarkdown(
+//                                podcastFile.getIntermediateConvertedFile(),
+//                                podcastFile.getBasicElement(),
+//                                podcastFile.getAsciidocAudioTag(),
+//                                podcastFile.getAsciidocDonwloadTag(),
+//                                podcastFile.getAsciidocImageTag()));
+//                        podcastFile.setAsciidocWithHtml(prepareAsciidocStringConvertedFromMarkdown(
+//                                podcastFile.getIntermediateConvertedFile(),
+//                                podcastFile.getBasicElement(),
+//                                podcastFile.getHtmlAudioTag(),
+//                                podcastFile.getHtmlDonwloadTag(),
+//                                podcastFile.getHtmlImageTag()));
+//                        break;
+//                    case Constants.HTML:
+//                        podcastFile = new PodcastFile(file);
+//                        from = "html";
+//                        command = String.format(commandPattern, from, podcastFile.getOutputFile(), input).split(" ");
+//                        executePandocCommand(command);
+//                        podcastFile.setAsciidocWithoutHtml(prepareAsciidocStringConvertedFromHtml(
+//                                podcastFile.getIntermediateConvertedFile(),
+//                                podcastFile.getBasicElement(),
+//                                podcastFile.getAsciidocAudioTag(),
+//                                podcastFile.getAsciidocDonwloadTag(),
+//                                podcastFile.getAsciidocImageTag()));
+//                        podcastFile.setAsciidocWithHtml(prepareAsciidocStringConvertedFromHtml(
+//                                podcastFile.getIntermediateConvertedFile(),
+//                                podcastFile.getBasicElement(),
+//                                podcastFile.getHtmlAudioTag(),
+//                                podcastFile.getHtmlDonwloadTag(),
+//                                podcastFile.getHtmlImageTag()));
+//                        break;
+//                    default:
+//                        break;
+//                }
+//                if(podcastFile != null) {
+//                    saveContentToFile(podcastFile.getAsciidocWithHtml(), podcastFile.getConvertedFileWithHtml());
+//                    saveContentToFile(podcastFile.getAsciidocWithoutHtml(), podcastFile.getConvertedFileWithoutHtml());
+//                }
+//            }
+//        }
+//    }
+//
+//    private static String prepareAsciidocStringConvertedFromHtml(
+//            File file,
+//            String basicElement,
+//            String audioTag,
+//            String downloadTag,
+//            String imageTag) throws IOException {
+//        StringBuilder sb = new StringBuilder();
+//        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+//            int countBasicElement = 0;
+//            for(String line; (line = br.readLine()) != null; ) {
+//                if(line.contains("---") && countBasicElement < 2) {
+//                    countBasicElement++;
+//                } else if(countBasicElement == 2) {
+//                    if(checkTagNotNull(basicElement, "basic element", file)) {
+//                        sb.append(basicElement).append("\n");
+//                    }
+//                    if(checkTagNotNull(imageTag, "image", file)) {
+//                        sb.append(imageTag).append("\n");
+//                    }
+//                    countBasicElement++;
+//                } else if(countBasicElement >= 2) {
+//                    if (line.matches("(\\[cols=\"\",\\])")) {
+//                        if(checkTagNotNull(downloadTag, "download", file)) {
+//                            sb.append(downloadTag).append("\n");
+//                        }
+//                    } else if (line.matches("(Your browser does not support the audio tag\\.)")) {
+//                        if(checkTagNotNull(audioTag, "audioTag", file)) {
+//                            sb.append(audioTag).append("\n");
+//                        }
+//                    } else if (!line.matches("^\\|.+")) {
+//                        sb.append(line).append("\n");
+//                    }
+//                }
+//
+//            }
+//        }
+//        return sb.toString();
+//    }
+//    private static String prepareAsciidocStringConvertedFromMarkdown(
+//            File file,
+//            String basicElement,
+//            String audioTag,
+//            String downloadTag,
+//            String imageTag) throws IOException {
+//        StringBuilder sb = new StringBuilder();
+//        if(checkTagNotNull(basicElement, "base element", file)) {
+//            sb.append(basicElement).append("\n");
+//        }
+//        if(checkTagNotNull(imageTag, "image", file)) {
+//            sb.append(imageTag).append("\n");
+//        }
+//        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+//            for(String line; (line = br.readLine()) != null; ) {
+//                if(line.matches("\\s?(Your browser does not support the audio tag\\.)")) {
+//                    if(checkTagNotNull(audioTag, "audioTag", file)) {
+//                        sb.append(audioTag).append("\n");
+//                    }
+//                    if(checkTagNotNull(downloadTag, "download", file)) {
+//                        sb.append(downloadTag).append("\n");
+//                    }
+//                } else {
+//                    sb.append(line).append("\n");
+//                }
+//
+//            }
+//        }
+//        return sb.toString();
+//    }
+//    private static boolean checkTagNotNull(String tag, String tagName, File file) {
+//        if(tag == null) {
+//            LOG.info("File {} has no {} tag", FilenameUtils.getBaseName(file.getName()), tagName);
+//            return false;
+//        } else {
+//            return true;
+//        }
+//    }
+//    private static void executePandocCommand(String[] command) throws IOException, InterruptedException {
+//        ProcessBuilder builder = new ProcessBuilder(command);
+//        builder.redirectErrorStream(true);
+//        Process p = builder.start();
+//        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//        String line;
+//        while (true) {
+//            line = r.readLine();
+//            if (line == null) {
+//                break;
+//            }
+//            System.out.println(line + " " + command[command.length - 1]);
+//        }
+//        p.waitFor();
+//    }
+//    private static void saveContentToFile(String content, File file) throws IOException {
+//        file.createNewFile();
+//        try(FileOutputStream fos = new FileOutputStream(file)) {
+//            fos.write(content.getBytes());
+//        }
+//    }
+//    private static void saveBasicElementToFile(File podcastFile, File asciidocFile) throws IOException,
+//            URISyntaxException {
+//        StatisticParser parser = new StatisticParser();
+//        long length = parser.getMP3FileLenght(podcastFile);
+//        LocalDate ld = FileParser.getPodcastDate(podcastFile);
+//        String convertedBasicElement = findBasicElementAsciidoc(asciidocFile);
+//        long podcastId = FileParser.getPodcastId(podcastFile);
+//        if(convertedBasicElement == null || ld == null) {
+//            System.out.println(String.format("Basic element of the file %s is not exists", asciidocFile.getName()));
+//        } else {
+//            Pattern pattern = Pattern.compile("\".+\"");
+//            Matcher matcher = pattern.matcher(convertedBasicElement);
+//            String convertedDocumentTitle = null;
+//            if(matcher.find()) {
+//                convertedDocumentTitle = matcher.group().split("\"")[1];
+//            }
+//            String basicElement = String.format(
+//                    basicAsciidocElementBuilder(),
+//                    podcastId,
+//                    length,
+//                    convertedDocumentTitle,
+//                    ld.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+//                    parser.getMp3Filename(podcastFile));
+//            File file = new File(String.format("basic-elements/%s-basic.txt",
+//                    FilenameUtils.getBaseName(podcastFile.getName())));
+//            if(file.createNewFile()) {
+//                try(FileOutputStream fos = new FileOutputStream(file)) {
+//                    fos.write(basicElement.getBytes());
+//                }
+//            }
+//        }
+//    }
+//private static String findBasicElementAsciidoc(File asciidocFile) {
+//    Asciidoctor asciidoctor = Asciidoctor.Factory.create();
+//    List<ContentPart> parts = asciidoctor.readDocumentStructure(asciidocFile, new HashMap<>()).getParts();
+//    if(parts.isEmpty()) {
+//        return null;
+//    } else {
+//        return parts.get(0).getContent();
+//    }
+//}
+}
---
+++ src/main/java/org/avlasov/razborpoletov/reader/converters/TagsBuilder.java
@@ -0,0 +1,72 @@
+package org.avlasov.razborpoletov.reader.converters;
+
+/**
+ * Created by artemvlasov on 04/06/15.
+ */
+public class TagsBuilder {
+    public static String basicAsciidocElementBuilder(long podcastId,
+                                                      long mp3FileLength,
+                                                      String podcastTitle,
+                                                      String podcastDate,
+                                                      String mp3FileName) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("---").append("\n");
+        sb.append("comments: true").append("\n");
+        sb.append("categories:").append("\n");
+        sb.append(String.format("uid: %d", podcastId)).append("\n");
+        sb.append(String.format("length: %d", mp3FileLength)).append("\n");
+        sb.append("---").append("\n");
+        sb.append(String.format("= %s", podcastTitle)).append("\n");
+        sb.append(String.format("%s", podcastDate)).append("\n");
+        sb.append(":layout: post").append("\n");
+        sb.append(String.format(":filename: %s", mp3FileName)).append("\n");
+        return sb.toString();
+    }
+    public static String audioTagHtmlBuilder(String mp3Url) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("++++\n");
+        sb.append("<!-- player goes here-->\n");
+        sb.append("<audio preload=\"none\">\n");
+        sb.append(String.format("<source src=\"%s\" type=\"audio/mp3\" />\n", mp3Url));
+        sb.append("Your browser does not support the audio tag.\n");
+        sb.append("</audio>\n");
+        sb.append("++++\n");
+        return sb.toString();
+    }
+    public static String audioTagAsciidocBuilder(String mp3Url) {
+        return String.format("audio::%s[]", mp3Url);
+    }
+    public static String downloadTagHtmlBuilder(String mp3Url) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("++++\n");
+        sb.append("<!-- episode file link goes here-->\n");
+        sb.append(String.format("<a href=\"%s\" imageanchor=\"1\" " +
+                "style=\"clear: left; margin-bottom: 1em; margin-left: auto; margin-right: 2em;\">\n", mp3Url));
+        sb.append("<img border=\"0\" height=\"64\" src=\"http://2.bp.blogspot" +
+                ".com/-qkfh8Q--dks/T0gixAMzuII/AAAAAAAAHD0/O5LbF3vvBNQ/s200/1330127522_mp3.png\" width=\"64\"/>\n");
+        sb.append("</a>\n");
+        sb.append("++++\n");
+        return sb.toString();
+    }
+    public static String downloadTagAsciidocBuilder(String mp3Url) {
+        return String.format("image::http://2.bp.blogspot" +
+                ".com/-qkfh8Q--dks/T0gixAMzuII/AAAAAAAAHD0/O5LbF3vvBNQ/s200/1330127522_mp3" +
+                ".png[link=\"%s\" width=\"64\" height=\"64\"]\n", mp3Url);
+    }
+    public static String imageTagHtmlBuilder(String imgUrl) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("++++\n");
+        sb.append("<div class=\"separator\" style=\"clear: both; text-align: center;\">\n");
+        sb.append(String.format("<a href=\"%s\" imageanchor=\"1\" " +
+                "style=\"margin-left: 1em; margin-right: 1em;\">\n", imgUrl));
+        sb.append(String.format("<img border=\"0\" height=\"350\" src=\"%s\" width=\"350\" />\n", imgUrl));
+        sb.append("</a>\n");
+        sb.append("</div>\n");
+        sb.append("++++\n");
+        return sb.toString();
+    }
+    public static String imageTagAsciidocBuilder(String imgUrl) {
+        return String.format("image::%s[width=\"350\" height=\"350\"" +
+                " link=\"%s\" align=\"center\"]\n", imgUrl, imgUrl);
+    }
+}
