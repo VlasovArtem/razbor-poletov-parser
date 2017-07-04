f781d8892da070b867ebc61bf92cf88b7c5f030c
Vlasov Artem <avlasov@selectica.com>
2017-07-04 19:41:36 +0300

    Add exception class for wrapping nested exceptions

---
+++ src/main/java/org/avlasov/razborpoletov/reader/exception/PodcastLinkParseException.java
@@ -0,0 +1,10 @@
+package org.avlasov.razborpoletov.reader.exception;
+
+/**
+ * Created by artemvlasov on 04/07/2017.
+ */
+public class PodcastLinkParseException extends RuntimeException {
+    public PodcastLinkParseException(String s) {
+        super(s);
+    }
+}
