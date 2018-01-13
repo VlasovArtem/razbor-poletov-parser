6211cd0de8dbcbfca78aa8e0e00a84f900efc230
Vlasov Artem <avlasov@selectica.com>
2018-01-13 16:44:26 +0200

    Update AppConfig with new CommandLine bean

--- src/main/java/org/avlasov/razborpoletov/reader/config/AppConfig.java
+++ src/main/java/org/avlasov/razborpoletov/reader/config/AppConfig.java
@@ -2,9 +2,9 @@ package org.avlasov.razborpoletov.reader.config;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.apache.commons.cli.CommandLine;
+import org.avlasov.razborpoletov.reader.utils.CLIUtils;
+import org.springframework.context.annotation.*;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 /**
@@ -12,11 +12,13 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
  */
 @Configuration
 @ComponentScan(basePackages = "org.avlasov")
+@PropertySource("classpath:twitter.properties")
 public class AppConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = Jackson2ObjectMapperBuilder.json()
+                .failOnUnknownProperties(false)
                 .autoDetectFields(true)
                 .autoDetectGettersSetters(false)
                 .build();
@@ -26,6 +28,12 @@ public class AppConfig {
                 .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
                 .withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
         return mapper;
+    }
+
+    @Bean
+    @Profile("commandLine")
+    public CommandLine commandLine(CLIUtils cliUtils) {
+        return cliUtils.createCommandLine();
     }
 
 }
