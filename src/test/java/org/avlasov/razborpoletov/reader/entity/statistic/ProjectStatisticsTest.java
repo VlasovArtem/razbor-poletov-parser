ae89d2091a3dbeae07424b92931905a4ba310572
Vlasov Artem <avlasov@selectica.com>
2018-01-17 23:25:15 +0200

    Update ProjectStatistics test

---
+++ src/test/java/org/avlasov/razborpoletov/reader/entity/statistic/ProjectStatisticsTest.java
@@ -0,0 +1,15 @@
+package org.avlasov.razborpoletov.reader.entity.statistic;
+
+import org.junit.Test;
+
+/**
+ * Created By artemvlasov on 17/01/2018
+ **/
+public class ProjectStatisticsTest {
+
+    @Test
+    public void defaultConstructor() {
+        new ProjectStatistics();
+    }
+
+}
